62a377c37ebece3534474c50992944b5a4faaee9
dmvillarroel2020 <dm.villarroel.2020@alumnos.urjc.es>
2023-03-20 20:31:28 +0100

    cambios

--- P2/practica2/metro.js
+++ P2/practica2/metro.js
@@ -17,8 +17,6 @@ const gui = {
 
 }
 
-
-
 console.log("Ejecutando JS...");
 //-- SECRET KEY Y MOSTRAR POR CONSOLE.LOG
 
@@ -42,38 +40,65 @@ for (let j = 0; j < secretkey.length; j++) {
     console.log( 'Posición nº' + j + ' Dígito Codificado ' + secretkey[j]);
 }
 
-gui.cod1.innerHTML = secretkey[0];
-gui.cod1.style.backgroundColor = "green";
+//-- Cambia de color e innerHTML los asteriscos
+{
+    //gui.cod1.innerHTML = secretkey[0];
+//gui.cod1.style.backgroundColor = "green";
 
-gui.cod2.innerHTML = secretkey[1];
-gui.cod2.style.backgroundColor = "green";
+//gui.cod2.innerHTML = secretkey[1];
+//gui.cod2.style.backgroundColor = "green";
 
-gui.cod3.innerHTML = secretkey[2];
-gui.cod3.style.backgroundColor = "green";
+//gui.cod3.innerHTML = secretkey[2];
+//gui.cod3.style.backgroundColor = "green";
 
-gui.cod4.innerHTML = secretkey[3];
-gui.cod4.style.backgroundColor = "green";
-
+//gui.cod4.innerHTML = secretkey[3];
+//gui.cod4.style.backgroundColor = "green";
+}
 
 //-- CRONOMETRO
 
-    //-- Definir un objeto cronómetro
+//-- Definir un objeto cronómetro
 const crono = new Crono(gui.display);
 
-
-
-
+//-- /Definimos un objeto arrayteclado
+const arrayteclado = [];
 
 //---- Configurar las funciones de retrollamada
 
-
 //-- Arranque del cronometro al pulsar un boton numerico
-for (let k =0; k < 10; k++) {
+for (let k = 0; k < 10; k++) {
     gui.numero[k].onclick = () => {
     crono.start();
-}
+    
+    //-- Guarda el click en numarray
+    let numarray = gui.numero[k].onclick();
+    arrayteclado.push(numarray.toString());
+    }
 }
 
+//-- If y for si un valor del array del arrayteclado está en 
+//-- el array de secretkey entonces hace innerHTML y cambia el color
+var found = false;
+
+for (var l = 0; l < arrayteclado.length; l++) {
+    if (secretkey.indexOf(arrayteclado[l]) > -1) {
+
+        found = true;
+        
+        gui.cod1.innerHTML = secretkey[l];
+        gui.cod1.style.backgroundColor = "green";
+
+        gui.cod2.innerHTML = secretkey[l];
+        gui.cod2.style.backgroundColor = "green";
+
+        gui.cod3.innerHTML = secretkey[l];
+        gui.cod3.style.backgroundColor = "green";
+
+        gui.cod4.innerHTML = secretkey[l];
+        gui.cod4.style.backgroundColor = "green";
+        break;
+    }
+}
 
 //-- Arranque del cronometro
 gui.start.onclick = () => {
@@ -81,10 +106,15 @@ gui.start.onclick = () => {
     crono.start();
 }
 
-//-- Detener el cronómetro
+//-- Detener el cronómetro y detener el cronometro si la contraseña es correcta
 gui.stop.onclick = () => {
     console.log("Stop!");
     crono.stop();
+    
+    if (arrayteclado[l] === secretkey[j]) {
+        crono.stop();
+        console.log("CONTRASEÑA CORRECTA!");
+    }
 }
 
 //-- Reset del cronómetro
